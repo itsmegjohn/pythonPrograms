4f8f4af4ee177374142106509d6f544317586854
George John <itsmegjohn@gmail.com>
2023-07-17 07:58:52 +0530

    converting days to years and weeks added

---
+++ daysToYears.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+days = int(input("Enter the number of days:"))
+years = days // 365
+weeks = (days % 365)//7
+remainingDays = days - years*365 - weeks*7
+
+print(years, "Years", weeks, "Weeks", remainingDays, "days")
